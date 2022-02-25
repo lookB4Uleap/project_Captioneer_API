9823f05ef0e3d611b3a9cf9a05a3da472d922ace
lB4Ul <anish.sarkar.official@gmail.com>
2022-02-25 16:12:52 +0530

    changed project structure

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-import os, shutil
+import os
+import shutil
 from flask import Flask, request, render_template, jsonify
 from flask_cors import CORS
 # import tensorflow as tf
@@ -17,13 +18,7 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tqdm import tqdm
 
 
-vocab = np.load('./vocab.npy', allow_pickle=True)
-vocab = vocab.item()
-inv_vocab = {v:k for k,v in vocab.items()}
 
-embedding_size = 128
-vocab_size = len(vocab)
-max_len = 35
 
 # image_model = Sequential()
 
@@ -47,67 +42,78 @@ max_len = 35
 
 # model.compile(loss='categorical_crossentropy', optimizer='RMSprop', metrics=['accuracy'])
 
-model = load_model('./image_caption_generator_model.h5')
-model.load_weights('./model_weights.h5')
-
-resnet = load_model('./resnet50.h5')
-
 # , static_url_path = "/app/static", static_folder = "static"
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 1
 CORS(app)
 
-folder = 'static/'
+folder = 'static/images/'
+
 
 @app.route("/")
 def hello_world():
-    return "<h1>Hello, World! " + str(vocab_size) + "</h1>"
+    return "<h1>Hello, World!</h1>"
 
-@app.route('/api/generate-caption', methods = ['GET', 'POST'])
+
+@app.route('/api/generate-caption', methods=['GET', 'POST'])
 def generate_caption():
-  if request.method == 'POST' :
-    f = request.files['image']
-    for filename in os.listdir(folder):
-      file_path = os.path.join(folder, filename)
-      try:
-        if os.path.isfile(file_path) or os.path.islink(file_path):
-            os.unlink(file_path)
-        elif os.path.isdir(file_path):
-            shutil.rmtree(file_path)
-      except Exception as e:
-        print('Failed to delete %s. Reason: %s' % (file_path, e))
-    f.save('static/' + f.filename)
-    # print(f)
+    if request.method == 'POST':
 
-    global model, resnet, vocab, inv_vocab, max_len, embedding_size
-    image = cv2.imread('static/' + str(f.filename))
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    image = cv2.resize(image, (224,224))
-    image = np.reshape(image, (1,224,224,3))
+        vocab = np.load('static/models/vocab.npy', allow_pickle=True)
+        vocab = vocab.item()
+        inv_vocab = {v: k for k, v in vocab.items()}
 
-    incept = resnet.predict(image).reshape(1,2048)
+        embedding_size = 128
+        vocab_size = len(vocab)
+        max_len = 35
 
-    text_inp = ['startofseq']
+        model = load_model('static/models/image_caption_generator_model.h5')
+        model.load_weights('static/models/model_weights.h5')
 
-    count = 0
-    caption = ''
-    while tqdm(count < 25):
-        count += 1
-        encoded = []
-        for i in text_inp:
-            encoded.append(vocab[i])
-        # encoded = [encoded]
-        # encoded = pad_sequences(encoded, padding='post', truncating='post', maxlen=MAX_LEN)
-        padded = pad_sequences([encoded], maxlen=max_len, padding='post', truncating='post').reshape(1,max_len)
-        prediction = np.argmax(model.predict([incept, padded]))
-        sampled_word = inv_vocab[prediction]
-            
-        if sampled_word != 'endofseq':
-            caption = caption + ' ' + sampled_word
+        resnet = load_model('static/models/resnet50.h5')
+        f = request.files['image']
+        for filename in os.listdir(folder):
+            file_path = os.path.join(folder, filename)
+            try:
+                if os.path.isfile(file_path) or os.path.islink(file_path):
+                    os.unlink(file_path)
+                elif os.path.isdir(file_path):
+                    shutil.rmtree(file_path)
+            except Exception as e:
+                print('Failed to delete %s. Reason: %s' % (file_path, e))
+        f.save(folder + f.filename)
+        # print(f)
+        
+        image = cv2.imread(folder + str(f.filename))
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        image = cv2.resize(image, (224, 224))
+        image = np.reshape(image, (1, 224, 224, 3))
 
-        text_inp.append(sampled_word)
+        incept = resnet.predict(image).reshape(1, 2048)
 
-    return jsonify({
-      'name'  : f.filename,
-      'caption' : caption
-    })
+        text_inp = ['startofseq']
+
+        count = 0
+        caption = ''
+        while tqdm(count < 25):
+            count += 1
+            encoded = []
+            for i in text_inp:
+                encoded.append(vocab[i])
+            # encoded = [encoded]
+            # encoded = pad_sequences(encoded, padding='post', truncating='post', maxlen=MAX_LEN)
+            padded = pad_sequences(
+                [encoded], maxlen=max_len, padding='post', truncating='post').reshape(1, max_len)
+            prediction = np.argmax(model.predict([incept, padded]))
+            sampled_word = inv_vocab[prediction]
+
+            if sampled_word != 'endofseq':
+                caption = caption + ' ' + sampled_word
+
+            text_inp.append(sampled_word)
+
+        return jsonify({
+            'name': f.filename,
+            'path': folder + f.filename,
+            'caption': caption
+        })
